24e03a712d9e22156c1a2a6b931ba59f2ee15960
emporteme <magzhankarataev02@gmail.com>
2023-03-30 12:39:02 +0600

    Basic layout

---
+++ components/MainLayout.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from "react";
+import Head from "next/head";
+// import Header from "./Header";
+// import Footer from "./Footer";
+import ScrollToTop from "./ScrollToTop";
+
+interface MainLayoutProps {
+    children: ReactNode;
+    title?: string;
+    spacing: string;
+}
+
+export default function MainLayout({ children, spacing = "2vw", title = "Diploma | Universities in Kazakhstan" }: MainLayoutProps) {
+    return (
+        <div>
+            <Head>
+                <title>{title}</title>
+            </Head>
+            <div>
+               
+                <main style={{ padding: spacing }}>
+                    {children}
+                    <ScrollToTop />
+                </main>
+                
+            </div>
+        </div>
+    );
+}
