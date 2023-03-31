e5d37e1d5a8099099db4ed917dfd672edd04de39
emporteme <magzhankarataev02@gmail.com>
2023-03-31 12:36:28 +0600

    new page at header

--- components/Header.tsx
+++ components/Header.tsx
@@ -32,8 +32,9 @@ export default function Header() {
                 <div className={showLinks ? `${styles.links} ${styles.show}` : `${styles.links}`}>
                     <ul className="mainText">
                         <li className={router.pathname === '/' ? `${styles.activeHeader}` : ''}><Link href="/">Home</Link></li>
-                        <li className={router.pathname === '/books' ? `${styles.activeHeader}` : ''}><Link href="/universities">Books</Link></li>
+                        <li className={router.pathname === '/books' ? `${styles.activeHeader}` : ''}><Link href="/books">Books</Link></li>
                         <li className={router.pathname === '/recomendations' ? `${styles.activeHeader}` : ''}><Link href="/recomendations">Recomendations</Link></li>
+                        <li className={router.pathname === '/reviews' ? `${styles.activeHeader}` : ''}><Link href="/reviews">Reviews</Link></li>
                         <li className={router.pathname === '/aboutUs' ? `${styles.activeHeader}` : ''}><Link href="/aboutUs">About us</Link></li>
                         <li className={router.pathname === '/contacts' ? `${styles.activeHeader}` : ''}><Link href="/contacts">Contacts</Link></li>
                     </ul>
