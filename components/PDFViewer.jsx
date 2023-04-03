d63f923eaf4e4712902ea42eb6cb0e22e3d39532
emporteme <magzhankarataev02@gmail.com>
2023-04-03 10:03:46 +0600

    book details page enabled to read a book

---
+++ components/PDFViewer.jsx
@@ -0,0 +1,42 @@
+// components/PDFViewer.js
+import { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+const PDFViewer = ({ file }) => {
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+
+    const onDocumentLoadSuccess = ({ numPages }) => {
+        setNumPages(numPages);
+    };
+
+    const nextPage = () => {
+        if (pageNumber < numPages) {
+            setPageNumber(pageNumber + 1);
+        }
+    };
+
+    const prevPage = () => {
+        if (pageNumber > 1) {
+            setPageNumber(pageNumber - 1);
+        }
+    };
+
+    return (
+        <div>
+            <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
+                <Page pageNumber={pageNumber} />
+            </Document>
+            <p>
+                Page {pageNumber} of {numPages}
+            </p>
+            <button onClick={prevPage}>Previous</button>
+            <button onClick={nextPage}>Next</button>
+        </div>
+    );
+};
+
+export default PDFViewer;
--- pages/books/[id].jsx
+++ pages/books/[id].jsx
@@ -2,20 +2,25 @@
 import MainLayout from "@/components/MainLayout"
 import mockBooks from '../../data/books'
 import { useRouter } from 'next/router'
+// PDF
+import PDFViewer from '../../components/PDFViewer';
 
-export default function BookDetails() {
-    // Checking the routes 
-    const router = useRouter()
+const getBookById = (id) => {
+    return mockBooks.find((book) => book.id.toString() === id);
+};
+
+const BookDetail = () => {
+    const router = useRouter();
     const { id } = router.query;
-    const bookId = parseInt(id, 10); // convert id to a number with base 10
 
-    const book = mockBooks.find((book) => book.id === bookId);
-
+    const book = getBookById(id);
 
     if (!book) {
-        console.log(book)
-        return (<MainLayout><div>Book not found</div></MainLayout>)
+        return <MainLayout>Book not found</MainLayout>;
     }
+
+    const fileUrl = `/${book.fileUrl}`;
+
     return (
         <MainLayout>
             <div>
@@ -23,7 +28,19 @@ export default function BookDetails() {
                 <h1>{book.title}</h1>
                 <p>by {book.author}</p>
                 <p>{book.description}</p>
+                <PDFViewer file={book.fileUrl} />
             </div>
         </MainLayout>
-    )
-}
+    );
+};
+
+export default BookDetail;
+
+{/* <MainLayout>
+<div>
+    <img src={book.image} alt={book.title} />
+    <h1>{book.title}</h1>
+    <p>by {book.author}</p>
+    <p>{book.description}</p>
+</div>
+</MainLayout> */}
