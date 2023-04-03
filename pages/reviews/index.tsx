f509f7456abb6863b02d95695f8c661d979e2070
emporteme <magzhankarataev02@gmail.com>
2023-04-03 18:29:52 +0600

    Reviews Page started

--- pages/reviews/index.tsx
+++ pages/reviews/index.tsx
@@ -1,8 +1,24 @@
 import MainLayout from "@/components/MainLayout"
+import reviews from "@/data/reviews"
+import styles from "../../styles/review.module.scss"
+
+
 export default function Reviews() {
     return (
         <MainLayout>
-            MOCK PAGE
+            <div className="subHeader">Reviews:</div>
+            <div className={styles.wrapper}>
+                {reviews.map((review) => {
+                    return (
+                        <div key={review.id} className={styles.review}>
+                            <div> <b>{review.username}</b> </div>
+                            <div> <i>{review.comment}</i> </div>
+                            <div>rating: {review.rating}</div>
+                            <div>{review.date}</div>
+                        </div>
+                    )
+                })}
+            </div>
         </MainLayout>
     )
 }
