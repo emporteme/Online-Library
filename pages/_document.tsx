730f3e97fc5a545b632c3b6ecb323b61c4bfce97
emporteme <magzhankarataev02@gmail.com>
2023-04-03 17:47:52 +0600

    Magzhan Krasavchik

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="keywords" content="Online, Online library, Library, Diploma,Kazakhstan,University,Colledge,Education,Magzhan Karatayev,Magzhan,Karatayev,Astana IT University,aitu,MT,mt,Media Technologies,next.js" />
-        <meta name="description" content="Online library, This is my graduation project at Astana IT University for a bachelor's degree." />
+        <meta name="description" content="Online library, Magzhan" />
         <meta charSet="utf-8" />
       </Head>
       <body>
