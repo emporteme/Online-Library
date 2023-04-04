417b1a9071bb8e82f36244ca83eba21befc1212e
emporteme <magzhankarataev02@gmail.com>
2023-04-04 11:59:59 +0600

    header changes

--- pages/recomendations/index.jsx
+++ pages/recomendations/index.jsx
@@ -1,8 +1,18 @@
 import MainLayout from "@/components/MainLayout"
+import mockBooks from "@/data/books"
+
+
 export default function Recomendations() {
     return (
         <MainLayout>
             MOCK PAGE
+            {mockBooks.map((book) => {
+                return (
+                    <div key={book.id}>
+                        {book.title}
+                    </div>
+                )
+            })}
         </MainLayout>
     )
 }
